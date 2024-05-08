c844ca77cd78998e3ef61f9900c0c90836dc64fc
Abdullah <abdallahmosbah25@gmail.com>
2024-05-08 17:30:42 +0300

    Task 1 fix02

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """return the value in self.cache_data if exists else None"""
-        try:
-            return self.cache_data[key]
-        except ValueError:
-            return None
+        return self.cache_data.get(key, None)
